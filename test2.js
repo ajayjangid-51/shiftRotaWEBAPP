b53b5cb59c0bc1fec8e1995b0333f2b8c25fbef5
A J <aj@solvorry.com>
2026-02-03 13:41:57 +0530

    second commit

--- test2.js
+++ test2.js
@@ -1,48 +1,44 @@
-//
-gita - SSA - 2;
-savitha - SSA - 2;
-janhavi - SA - 1;
-christina - SA - 2;
-
-vikas - SSA - 1;
-prajakta - SSA - 2;
-vaishali - SA - 1;
-arushina - SA - 2;
-
-sarvesh - L1;
-prabhakar - L2;
-balachander - L3;
-santosh - SSA - 1;
-shubham - kadam - SSA - 2;
-ajay - jangid - SA - 1;
-srikant - SA - 1;
-mankala - SA - 1;
-sudarshan - SA - 1;
-chirag - SA - 1;
-sushant - SA - 2;
-abhishek - mehta - SA - 2;
-
-shailender - L1;
-rahul - L2;
-harsh - fatnani - L3;
-sachin - SSA - 1;
-somesh - SSA - 1;
-gaurav - shinde - SSA - 2;
-gaurav - rathva - SA - 1;
-vivek - pal - SA - 1;
-krishna - reddy - SA - 1;
-amarnath - SA2;
-praneeth - SA2;
-kavin - SA2;
-
-praveen - L1;
-(abhishek, -L2);
-aniket - SSA - 1;
-akshay - SSA - 2;
-mohan - bhor - SA - 1;
-pratik - SA - 1;
-anshul - SA - 2;
-arasan - SA - 2;
-sushant - SA - 2;
-aravinth - SA - 2;
-ajay - v - SA - 2;
+//                                  |S M T W T F S|
+gita - SSA - 2;                     |W M M M M M W|
+savitha - SSA - 2;                  |W M M M M M W|
+janhavi - SA - 1;                   |W M M M M M W|
+christina - SA - 2;                 |W M M M M M W|
+vikas - SSA - 1;                    |W A A A A A W|
+prajakta - SSA - 2;                 |W A A A A A W|
+vaishali - SA - 1;                  |W A A A A A W|
+arushina - SA - 2;                  |W A A A A A W|
+sarvesh - L1;                       |W M M M M M W|
+prabhakar - L2;                     |W M M M M M W|
+balachander - L3;                   |W M M M M M W|
+santosh - SSA - 1;                  |W M M M M M W|
+shubham - kadam - SSA - 2;          |W M M M M M W|
+ajay - jangid - SA - 1;             |W M M M M M W|
+srikant - SA - 1;                   |W M M M M M W|
+mankala - SA - 1;                   |W M M M M M W|
+sudarshan - SA - 1;                 |W M M M M M W|
+chirag - SA - 1;                    |W M M M M M W|
+sushant - SA - 2;                   |W M M M M M W|
+abhishek - mehta - SA - 2;          |W M M M M M W|
+shailender - L1;                    |W A A A A A W|
+rahul - L2;                         |W A A A A A W|
+harsh - fatnani - L3;               |W A A A A A W|
+sachin - SSA - 1;                   |W A A A A A W|
+somesh - SSA - 1;                   |W A A A A A W|
+gaurav - shinde - SSA - 2;          |W A A A A A W|
+gaurav - rathva - SA - 1;           |W A A A A A W|
+vivek - pal - SA - 1;               |W A A A A A W|
+krishna - reddy - SA - 1;           |W A A A A A W|
+amarnath - SA2;                     |W A A A A A W|
+praneeth - SA2;                     |W A A A A A W|
+kavin - SA2;                        |W A A A A A W|
+praveen - L1;                       |N N N N N N W|
+(abhishek, -L2);                    |N N N N N N W|
+aniket - SSA - 1;                   |N N N N N N W|
+akshay - SSA - 2;                   |N N N N N N W|
+mohan - bhor - SA - 1;              |N N N N N N W|
+pratik - SA - 1;                    |N N N N N N W|
+anshul - SA - 2;                    |N N N N N N W|
+arasan - SA - 2;                    |N N N N N N W|
+sushant - SA - 2;                   |N N N N N N W|
+aravinth - SA - 2;                  |N N N N N N W|
+ajay - v - SA - 2;                  |N N N N N N W|
